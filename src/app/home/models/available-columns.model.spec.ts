5c3faca50ed660981e7551d189bb8f2a71e2de52
Wojtek <golewski.wojciech@outlook.com>
2020-04-21 22:39:16 +0200

    generated available-columns model

---
+++ src/app/home/models/available-columns.model.spec.ts
@@ -0,0 +1,7 @@
+import { AvailableColumns } from './available-columns.model';
+
+describe('AvailableColumns', () => {
+  it('should create an instance', () => {
+    expect(new AvailableColumns()).toBeTruthy();
+  });
+});
